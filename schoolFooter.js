ccba545225c7206ea5e3bb7a517280a44011e81f
Yuvali Leibovich <yuvali.leibovich475@student.mckinneyisd.net>
2019-10-04 20:46:52 -0500

    colors changes

--- schoolFooter.js
+++ schoolFooter.js
@@ -1,2 +1,3 @@
 document.getElementsByTagName("FOOTER")[0].innerHTML = "Call Us: (469) 302-3400 / "+
-"600 N. Lake Forest Dr., McKinney, TX 75071 / <a href=Band/index.html>Band Website</a> / <a href=Band/credits.html>Credits</a>";
+"600 N. Lake Forest Dr., McKinney, TX 75071 / <a href=Band/index.html>SLiK Website</a>";
+/// <a href=Band/credits.html>Credits</a>
