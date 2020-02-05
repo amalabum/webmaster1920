4fe799236b725d533f177dc19e9e5152289f47c2
marmaladetea <cameron.sigua@gmail.com>
2020-02-05 12:27:33 -0600

    Testing navbar color change by section

--- Sleek/js/index.js
+++ Sleek/js/index.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
   $(window).scroll(function() {
-    if ($(document).scrollTop() < 60) {
+    if ($(document).scrollTop() < 60 ) {
      $("nav").removeClass("navbg");
+/*    This is me attempting to make the colors of the navbar to fit the color of what section its over
+   } else if   ($(document).scrollTop() > 630 && $(document).scrollTop() < 830) {
+      $("nav").removeClass("navbg");
+      $("nav").addClass("navbg2");
 
-    } else {
+   } else if   ($(document).scrollTop() > 1230 && $(document).scrollTop() < 1390) {
+      $("nav").removeClass("navbg");
+      $("nav").addClass("navbg3");
+*/
+   } else {
      $("nav").addClass("navbg");
 
     }
