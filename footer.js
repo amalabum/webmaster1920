28bf5a021da0248d45836293e8c0a58f5433ff5e
root <ka1wenkhb@gmail.com>
2020-02-28 23:20:19 +0000

    Footers

--- footer.js
+++ footer.js
@@ -1,3 +1,3 @@
 document.getElementsByTagName("FOOTER")[0].innerHTML = "Call Us: (469) 302-3400 "+
-"&nbsp&nbsp/&nbsp&nbsp600 N. Lake Forest Drive, McKinney, TX 75071&nbsp&nbsp/&nbsp&nbsp<a href=sleeek.ga>SLeeK Website </a>"+
+"&nbsp&nbsp/&nbsp&nbsp600 N. Lake Forest Drive, McKinney, TX 75071&nbsp&nbsp/&nbsp&nbsp<a href="sleeek.ga">SLeeK Website </a>"+
  "&nbsp&nbsp/&nbsp&nbsp<a href=credits.html>Credits</a>";
